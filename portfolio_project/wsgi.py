c244f72dcd2050ae431c478504a188b3fe93193a
Your Name <youremail@example.com>
2026-05-22 13:32:26 +0500

    logo fixed

--- portfolio_project/wsgi.py
+++ portfolio_project/wsgi.py
@@ -1,4 +1,11 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
+
+path = '/home/Abdull947/My-Protfolio'
+if path not in sys.path:
+    sys.path.append(path)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.settings')
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
