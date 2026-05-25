d4b11c610211652451357e349bfb9de90f66462a
Your Name <youremail@example.com>
2026-05-25 12:53:24 +0500

    admin text change

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
+admin.site.site_header = "Abdul Tech Admin"
+admin.site.site_title = "Abdul Tech Admin Portal"
+admin.site.index_title = "Welcome to Abdul Tech Admin Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio_app.urls')),
